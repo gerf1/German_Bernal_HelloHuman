4f76fbc9781c75ce81074359a83691c86f2b27b6
German Bernal <ger.e.bernal@gmail.com>
2019-08-06 21:59:58 -0500

    Add files via upload

---
+++ HelloHuman/src/main/java/com/codingdojo/human/HelloHuman.java
@@ -0,0 +1,23 @@
+package com.codingdojo.human;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HelloHuman {
+
+	@RequestMapping("/")
+	public String index(@RequestParam(value = "name", required = false) String name) {
+
+		if (name == null) {
+			return "Hello Human, Welcom to Spring Boot!";
+		} else {
+
+			return "Hello " + name + ", Welcom to Spring Boot!";
+
+		}
+	}
+
+}
